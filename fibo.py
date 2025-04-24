9a1d5a3b6081ae4322cacabd4b355a0373710e23
CamiloARG <camiloadian@gmail.com>
2025-04-24 12:26:09 +0200

    s

--- fibo.py
+++ fibo.py
@@ -1,7 +1,29 @@
+def recursive_nth_fibo(n):
+
+    number = 0
+    nas = []
+
+    while n > 0:
+        if n == 0:
+            number += 0
+            n -= 1
+            nas.append(number)
+        elif n == 1:
+            number += 1
+            n -= 1
+            nas.append(number)
+        else:
+            fib = (n-1) + (n-2)
+            number += fib
+            n -= 1
+            nas.append(number)
+
+    return nas
+
 
 def main():
-    pass
 
+    print(recursive_nth_fibo(3))
 
 if __name__ == "__main__":
     main()
